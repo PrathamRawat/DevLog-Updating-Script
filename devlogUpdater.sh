40197ebad2b5424a17413faad5ed8dd0d7e722d7
PrathamRawat <prathamrawat815@gmail.com>
2020-03-08 17:29:23 +0000

    add user input and validation

---
+++ devlogUpdater.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+timestamp() 
+{
+	time=$(date +%T)
+}
+
+# User input for first name
+echo "Enter your first name:"
+read first
+
+if [ ! $first ]
+then
+	echo "Please provide an input"
+	exit
+fi
+
+# User input for last name
+echo "Enter your last name:"
+read last
+
+if [ ! $last ]
+then
+	echo "Please provide an input"
+	exit
+fi
+
+# User input for update section
+echo "Enter your update:"
+read update
+
+if [ ! $update ]
+then
+	echo "Please provide an input"
+	exit
+fi
+
+# Convert last name to all lowercase
+last="$( echo $last | tr '[:upper:]' '[:lower:]')"
+# Convert firstname to all uppercase
+first="$( echo $first | tr '[:lower:]' '[:upper:]')"
+timestamp
+header="$last${first:0:1} -- $time"
+
+
